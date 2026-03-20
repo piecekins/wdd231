1b5b1790db0f38e2bc656f2fe743ca14a0868eb0
piecekins <rus24015@byui.edu>
2026-03-19 19:34:15 -0500

    prove week 111

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -1,29 +1,27 @@
+//add this at the top of the file
 import spritePath from '../images/sprite.symbol.svg';
 
-export function alertTemplate(info){
-  let id = info.category;
-  if (info.category === "park closure"){
-    return `<div class="alertCards">
-                <svg class="icon alert-closure" focusable="false" aria-hidden="true">
-                  <use xlink:href="${spritePath}#alert-closure"></use>
-                </svg>
-                <div>
-                  <h4 class="alert-closure" >${info.title}</h4>
-                  <p>${info.description}</p>
-                </div>
-            </div>`
-  }
-  else{
-    return `<div class="alertCards">
-                <svg class="icon alert-${id.toLowerCase()}" focusable="false" aria-hidden="true"><use xlink:href="${spritePath}#alert-${id.toLowerCase()}"></use></svg>
-                <div>
-                  <h4 class="alert-${id.toLowerCase()}">${info.title}</h4>
-                  <p>${info.description}</p>
-                </div>
-            </div>`
-  }
+// ...bunch of stuff
 
-
+export function alertTemplate(alert) {
+  let alertType = "";
+  // most of the alerts are one word and line up with the icons nicely. "Park Closure" is the exception
+  switch (alert.category) {
+    case "Park Closure":
+      alertType = "closure";
+      break;
+    default:
+      alertType = alert.category.toLowerCase();
+  }
+  // note the new path below for the SVG!
+  return `<li class="alert">
+  <svg class="icon" focusable="false" aria-hidden="true">
+    <use xlink:href="${spritePath}#alert-${alertType}"></use>  
+  </svg>
+  <div>
+    <h3 class="alert-${alertType}">${alert.title}</h3>
+    <p>${alert.description}</p>
+  </div></li>`;
 }
 
 export function footerTemplate(data){
